d4f24c836194d0ff8dea4cf01c04a9cf60873feb
artraf06 <artraf7506@gmail.com>
2026-09-07 10:38:36 +0200

    Wymuś aktualizację PWA po wycofaniu błędnej wersji

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,14 @@
-const CACHE="faktura-pwa-v1";self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["/","/manifest.webmanifest","/favicon.svg"]))));self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(a=>Promise.all(a.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
+const CACHE="faktura-pwa-v2";
+self.addEventListener("install",event=>{
+ self.skipWaiting();
+ event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(["/","/manifest.webmanifest","/favicon.svg"])));
+});
+self.addEventListener("activate",event=>event.waitUntil(
+ caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())
+));
+self.addEventListener("fetch",event=>{
+ if(event.request.method!=="GET")return;
+ event.respondWith(fetch(event.request).then(response=>{
+  const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;
+ }).catch(()=>caches.match(event.request)));
+});
